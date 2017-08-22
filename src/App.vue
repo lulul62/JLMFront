f1274c6b08121bc5e727c1c5cc83d0162f418f31
Leplan Ludwig <leplan.ludwig@gmail.com>
2017-08-22 21:05:16 +0200

    chore() : initialisation vue-cli

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title  v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+         <router-view></router-view>
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+    >
+      <v-list>
+        <v-list-tile @click="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed">
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [
+          { icon: 'directions_run', title: 'Carte des évenements', href : "/map" },
+           { icon: 'add_location', title: 'Créer mon évenement', href : "/createEvent" },
+            { icon: 'account_circle', title: 'Mon profil', href : "/profil" },
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="map">
+    <div id="map"></div>
+  </div>
+</template>
+
+<script defer>
+export default {
+  name: 'map',
+  data() {
+    return {
+      events: [],
+      conf: {}
+    }
+  },
+  methods: {
+    /**
+     * Init de la map sur le composant.
+     */
+    showMap() {
+      var map = L.map('map').setView([51.505, -0.09], 13);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    },
+
+    /**
+     * Récupere les URLS nécessaires au controleur. 
+     */
+    getConfig() {
+      return this.$http.get("static/conf.json").then(conf=>{
+         this.conf = conf.body;
+         return this.getAllEvents(this.conf.eventsBaseUrl);
+      });
+    },
+
+    /**
+     * Récupere la liste de tout les évenements en cours
+     */
+    getAllEvents(url) {
+      return this.$http.get(url).then(events => {
+         this.events = events.body;
+      });
+    }
+  },
+
+  mounted() { this.showMap(), this.getConfig()}
+}
+</script>
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body,
+#map {
+  height: 100vh;
+  z-index: 1;
+  width: 100vw;
+}
+</style>
+
+
+
